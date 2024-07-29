7fb1d2da5526678cf813c0f4514d0b8984e95663
godfatherc08 <139884797+godfatherc08@users.noreply.github.com>
2024-07-29 19:56:02 +0100

    Add files via upload

---
+++ question_2.js
@@ -0,0 +1,19 @@
+myStudent = ['Ade', 'Chimaobi', 'John', 'Oliver', 'Ebun', 'Judah', 'Ebuka', 'Lawson', 'Ope', 'Joe']
+
+
+var student = ""
+for( let student of myStudent ){
+    console.log(student)
+}
+
+object = {
+    "name": "Chimaobi",
+    "age": 16,
+    "married": false,
+    "location": undefined
+}
+
+console.log(object.name)
+console.log(object.age)
+console.log(object.married)
+console.log(object.location)
